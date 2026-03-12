84a134f400c9fe0fbde99b6653d2a2a4789729f4
gouf <goufril.mardini007@gmail.com>
2026-03-12 09:15:13 -0400

    Just files being created

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'BD/bd.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DarQuest</title>
 </head>
 <body>
     <h1>Hello World</h1>
