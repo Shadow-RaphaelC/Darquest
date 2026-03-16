07e419db59268d1da7561a70828158c2642de1a0
gouf <goufril.mardini007@gmail.com>
2026-03-16 14:46:31 -0400

    css test 3

--- include/header.php
+++ include/header.php
@@ -1,11 +1,5 @@
 <header>
     <div class="header">
         <img src="img/DarQuestTitle_WHITE.png" alt="DarQuest Logo" class="logo">
-        <nav>
-            <ul>
-                <li><a class="textTest" href="index.php">Home</a></li>
-            </ul>
-        </nav>
     </div>
 </header>
-
