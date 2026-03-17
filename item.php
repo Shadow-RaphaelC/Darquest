a7f86ae0d64430c0a146cd16c7d404603af172d5
gouf <goufril.mardini007@gmail.com>
2026-03-16 21:57:14 -0400

    debut item.php et quelques ajustements

---
+++ item.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Item placeholder</title>
+    <link rel="stylesheet" href="css/styles_dark.css">
+</head>
+
+<body>
+
+    <div class="itemBox">
+        <img src="img/placeholder.webp" alt="Product Image" class="item-img">
+
+        <div class="item-info">
+            <h2 class="prixOr">$120</h2>
+            <h3 class="titre">Overlord's bloodmail</h3>
+            <p class="description">
+                OVERLORD'S BLOODMAIL OVERLORD'S BLOODMAIL OVERLORD'S BLOODMAIL OVERLORD'S BLOODMAIL OVERLORD'S BLOODMAIL
+            </p>
+
+            <div class="btnPanier">
+                <img class="btnPanierImg" src="img/addToCart.png"></a>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
