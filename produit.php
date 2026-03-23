c249caa2140c5ec094a0d2003965d7933103cd41
gouf <goufril.mardini007@gmail.com>
2026-03-23 17:00:07 -0400

    produit.php + icon de + pour bouton achat - SVP FAIRE LES CHANGEMENTS ADEQUATS DANS bd.php AFIN D'AFFICHER LE BTN + CORRECTEMENT, LE CSS EST DEJA FAIT.

---
+++ produit.php
@@ -0,0 +1,35 @@
+<!--
+    CE DOCUMENT EST RESERVE POUR LES TESTES DE UI // NE PAS SUPPRIMER 
+  - Goufril Mardini 
+-->
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles_dark.css">
+    <title>DarQuest</title>
+</head>
+
+<body>
+    <div class="itemBox">
+    <div class="item-img-wrapper">
+        <img class="item-img" src="img/placeholder.webp" alt="placeholder">
+    </div>
+    <div class="item-info">
+        <h3 class="titre">placeholder</h3>
+        <p class="item-type">Type: placeholder</p>
+        <p class="description">Quantité: 67</p>
+        <p class="prixOr">67 gold</p>
+        <div class="btnPanier">
+            <a href="panier.php?action=add&id=67">
+                <img src="img/addToCart.png" class="btnPanierBtn" alt="Ajouter au panier">
+            </a>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
