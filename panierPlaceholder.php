c307dd55c77f079d0244d77d5bea2cf4d4b72d4c
gouf <goufril.mardini007@gmail.com>
2026-03-27 22:58:55 -0400

    Panier style and placeholder

---
+++ panierPlaceholder.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles_dark.css">
+    <title>DarQuest</title>
+</head>
+
+<body>
+    <div
+        style="display: flex; height: 100px; justify-content: space-between; align-items: center; border: 1px solid #ccc; padding: 10px; margin-bottom: 10px; border-radius: 5px; background-color: #00000079; width: 60%;">
+        <div style=" display: flex; justify-content: left; margin-right: 10px; gap: 10px; align-items: center;">
+            <img src="img/placeholder.webp" alt="placeholder"
+                style="width: 100px; height: 100px; object-fit: cover; border: 1px solid white; border-radius: 5px;">
+            <div style="align-items: left; max-height: 140 px; text-align: left;">
+                <h3> hello ! </h3>
+                <h3> prix : 67 gold </h3>
+            </div>
+        </div>
+        <div style="display: flex; justify-content: right;">
+            <div style="display: flex; align-items: center; gap: 5px;">
+                <button style="padding: 5px 10px;">-</button>
+                <span>1</span>
+                <button style="padding: 5px 10px;">+</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
