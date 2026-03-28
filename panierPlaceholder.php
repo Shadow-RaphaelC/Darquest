1c6634cae09e8de79edb13ce86b7c14c2d918b02
gouf <goufril.mardini007@gmail.com>
2026-03-27 23:25:58 -0400

    CSS adjustments and panier style in css page

--- panierPlaceholder.php
+++ panierPlaceholder.php
@@ -8,21 +8,19 @@
 </head>
 
 <body>
-    <div
-        style="display: flex; height: 100px; justify-content: space-between; align-items: center; border: 1px solid #ccc; padding: 10px; margin-bottom: 10px; border-radius: 5px; background-color: #00000079; width: 60%;">
-        <div style=" display: flex; justify-content: left; margin-right: 10px; gap: 10px; align-items: center;">
-            <img src="img/placeholder.webp" alt="placeholder"
-                style="width: 100px; height: 100px; object-fit: cover; border: 1px solid white; border-radius: 5px;">
-            <div style="align-items: left; max-height: 140 px; text-align: left;">
+    <div class="panier-item">
+        <div class="panier-item-left">
+            <img src="img/placeholder.webp" alt="placeholder" class="panier-item-img">
+            <div class="panier-item-left-info">
                 <h3> hello ! </h3>
                 <h3> prix : 67 gold </h3>
             </div>
         </div>
-        <div style="display: flex; justify-content: right;">
-            <div style="display: flex; align-items: center; gap: 5px;">
-                <button style="padding: 5px 10px;">-</button>
+        <div class="panier-item-right">
+            <div class="panier-item-quantity">
+                <button class="panier-item-btn">-</button>
                 <span>1</span>
-                <button style="padding: 5px 10px;">+</button>
+                <button class="panier-item-btn">+</button>
             </div>
         </div>
     </div>
